28787d6d3cdcb293c0d17965aa9fdddb0b5d17cd
Anand Kulkarni <anandkulkarni67@gmail.com>
2017-05-09 23:14:35 -0400

    add sample user entries

--- scripts/dml.sql
+++ scripts/dml.sql
@@ -1,7 +1,9 @@
 DELETE FROM COURSES;
+DELETE FROM USERS;
 
 COMMIT;
 
+-- sample courses.
 INSERT INTO COURSES VALUES ('c0001', 'CS', '520', 'Computer Architecture and Organ', 50);
 INSERT INTO COURSES VALUES ('c0002', 'CS', '526', 'Wireless Sensor Networks', 30);
 INSERT INTO COURSES VALUES ('c0003', 'CS', '528', 'Computer Networks', 38);
@@ -18,5 +20,12 @@ INSERT INTO COURSES VALUES ('c0013', 'CS', '580G', 'Game Devel for Mobile Platfo
 INSERT INTO COURSES VALUES ('c0014', 'CS', '580H', 'GUI and Windows Programming', 40);
 INSERT INTO COURSES VALUES ('c0015', 'CS', '580L', 'Intro to Machine Learning', 29);
 INSERT INTO COURSES VALUES ('c0016', 'CS', '580S', 'Smart Devices and Sensing', 34);
+
+-- sample users.
+-- password: password12345
+INSERT INTO USERS (USER_ID, USERNAME, PASSWORD, EMAIL, ROLE) VALUES (1001, 'coordinator', '365d38c60c4e98ca5ca6dbc02d396e53', 'coordinator@mail.edu', 'coordinator');
+INSERT INTO USERS (USER_ID, USERNAME, PASSWORD, EMAIL, ROLE) VALUES (1002, 'user1', '365d38c60c4e98ca5ca6dbc02d396e53', 'user1@mail.edu', 'student');
+INSERT INTO USERS (USER_ID, USERNAME, PASSWORD, EMAIL, ROLE) VALUES (1003, 'user2', '365d38c60c4e98ca5ca6dbc02d396e53', 'user2@mail.edu', 'student');
+INSERT INTO USERS (USER_ID, USERNAME, PASSWORD, EMAIL, ROLE) VALUES (1004, 'user3', '365d38c60c4e98ca5ca6dbc02d396e53', 'user3@mail.edu', 'student');
 
 COMMIT;
